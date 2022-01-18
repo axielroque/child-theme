968b793636f8dc8ea2a42b36c9b3983c893d6686
axielroque <axiel.roque@gmail.com>
2022-01-17 21:58:58 -0600

    [update] - files tree

--- functions.php
+++ functions.php
@@ -10,8 +10,8 @@ function child_theme_enqueue_styles() {
 	$the_theme = wp_get_theme();
 
     // Grab asset urls.
-    $theme_styles  = "/assets/css/main.css";
-    $theme_scripts = "/assets/js/main.js";
+    $theme_styles  = "/dist/css/main.css";
+    $theme_scripts = "/dist/js/main.js";
 
     wp_enqueue_style( 'child-theme-styles', get_stylesheet_directory_uri() . $theme_styles, array(), $the_theme->get( 'Version' ) );
 	wp_enqueue_script( 'jquery' );
@@ -28,7 +28,7 @@ add_action( 'wp_enqueue_scripts', 'child_theme_enqueue_styles' );
  * Load the child theme's text domain
  */
 function add_child_theme_textdomain() {
-	load_child_theme_textdomain( 'simpletheme', get_stylesheet_directory() . '/languages' );
+	load_child_theme_textdomain( '_s', get_stylesheet_directory() . '/languages' );
 }
 add_action( 'after_setup_theme', 'add_child_theme_textdomain' );
 
