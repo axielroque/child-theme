92a64da8e40c16ddf7b1d6c4f724b79e2b4b7bfe
Herald-Flores <heraldflores95@gmail.com>
2022-10-23 14:39:35 -0600

    feat: acf guttember block hero

--- blocks-acf/templates-blocks/hero/index.php
+++ blocks-acf/templates-blocks/hero/index.php
@@ -1,18 +1,59 @@
-<section class="bg-white dark:bg-gray-900">
-    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div class="mr-auto place-self-center lg:col-span-7">
-            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Payments tool for software companies</h1>
-            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.</p>
-            <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                Get started
-                <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </a>
-            <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                Speak to Sales
-            </a> 
+<?php
+
+// Support custom "anchor" values.
+$anchor = '';
+if (!empty($block['anchor'])) {
+  $anchor = 'id="' . esc_attr($block['anchor']) . '" ';
+}
+
+// Create class attribute allowing for custom "className" and "align" values.
+$class_name = 'testimonial-block';
+if (!empty($block['className'])) {
+  $class_name .= ' ' . $block['className'];
+}
+if (!empty($block['align'])) {
+  $class_name .= ' align' . $block['align'];
+}
+
+$title = get_field('title') ?: 'Title here...';
+$description = get_field('description') ?: 'description here';
+$image = get_field('image') ?: 295;
+$size = 'full'; // (thumbnail, medium, large, full or custom size)
+$mode = get_field('mode');
+$txt_align = get_field('align_text');
+?>
+
+<section id="<?php echo esc_attr($anchor); ?>" class="<?php echo esc_attr(
+  'dark' === $mode
+)
+  ? 'bg-gray-900'
+  : 'bg-white'; ?>">
+  <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 lg:py-16 lg:grid-cols-12">
+      <div class="mr-auto place-self-center lg:col-span-7 <?php echo 'left' ===
+      $txt_align
+        ? 'order-first'
+        : 'order-last'; ?>">
+        <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl <?php echo esc_attr(
+          'dark' === $mode
+        )
+          ? 'text-white'
+          : 'text-gray-900'; ?>">
+          <?php echo esc_html($title); ?>
+        </h1>
+        <div class="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl <?php echo esc_attr(
+          'dark' === $mode
+        )
+          ? 'text-gray-200'
+          : 'text-gray-600'; ?>">
+          <?php echo $description; ?>
         </div>
-        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-            <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup">
-        </div>                
-    </div>
+      </div>
+      <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+        <?php if (!empty($image)): ?>
+          <img src="<?php echo esc_url(
+            $image['url']
+          ); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+        <?php endif; ?>
+      </div>                
+  </div>
 </section>
--- home.php
+++ home.php
@@ -12,14 +12,9 @@
 get_header();
 
 /* Start the Loop */
-while ( have_posts() ) :
-	the_post();
-	get_template_part( 'template-parts/content/content-page' );
-
-	// If comments are open or there is at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) { 
-		comments_template();
-	}
+while (have_posts()):
+  the_post();
+  get_template_part('template-parts/content/content-page');
 endwhile; // End of the loop.
 
 get_footer();
